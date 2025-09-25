70201ceba7a23a22b5f8b7b5f8db196bbd72fe40
jla690 <jla690@sfu.ca>
2025-09-24 22:08:38 -0700

    DONE TUTORIAL

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup/></div>;
+  let [visibility, setVisibility] = useState(false);
+
+  return (
+    <div>
+      {visibility && <Alert onClick={() => setVisibility(false)}>Text</Alert>}
+      <Button onClick={() => setVisibility(true)}>Click here!</Button>
+    </div>
+  );
 }
 
 export default App;
--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -1,14 +1,32 @@
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+import { useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
+  const [selectedIndex, setIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>Empty List</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, i) => (
+          <li
+            className={
+              selectedIndex === i ? "list-group-item active" : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setIndex(i);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
